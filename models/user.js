02ca25e0b7d5177e2fbeb9fa676acedd9493849d
saloni jain <jsaloni484@gmail.com>
2020-04-26 16:53:39 +0530

    added file upload and saving file using params

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const multer=require('multer');
+const path=require('path');
+const AVTAR_PATH=path.join('/uploads/users/avtar');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    avtar:{
+        type:String
+    }
+}, {
+    timestamps: true
+});
+
+
+let storage=multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,path.join(__dirname,'..',AVTAR_PATH));
+    },
+
+    filename:function(req,file,cb){
+        cb(null,file.fieldname + '-' + Date.now());
+    }
+
+});
+
+userSchema.statics.uploadedAvtar=multer({storage:storage}).single('avtar');
+userSchema.statics.avtarPath=AVTAR_PATH;
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
