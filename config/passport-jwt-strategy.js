b9656d614abcefafcd059dca47dc9cb066975252
saloni jain <jsaloni484@gmail.com>
2020-05-07 22:04:12 +0530

    installing jsonwebtoken and creating token

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -1,27 +1,28 @@
-const passport=require('passport');
-const JWTSrategy=require('passport-jwt').Strategy;
+const passport = require('passport');
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
 
-const ExtractJWT=require('passport-jwt').ExtractJwt;
-const User=require('../models/user');
+const User = require('../models/user');
 
-let opts={
-    jwtFormRequest:ExtractJWT.fromAuthHeaderAsBearerToken,
-    secretOrKey='codeial'
+
+let opts = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken,
+    secretOrKey: 'codeial'
 }
 
-passport.use(new JWTSrategy(opts,function(jwtPayLoad,done){
 
-    User.findById(jwtPayLoad._id,function(err,done){
-        if(err){
-            console.log("error is finding user using jwt");
-            return (err);
-        }
-        if(user){
-            return done(null,user);
-        }
-        else{
-            return done(null,false);
-        }
+passport.use(new JWTStrategy(opts, function(jwtPayLoad, done){
 
+    User.findById(jwtPayLoad._id, function(err, user){
+        if (err){console.log('Error in finding user from JWT'); return;}
+
+        if (user){
+            return done(null, user);
+        }else{
+            return done(null, false);
+        }
     })
+
 }));
+
+module.exports = passport;
