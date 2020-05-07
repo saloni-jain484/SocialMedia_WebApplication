05f3b3636705b7e18e01f88a1d4045db117671f8
saloni jain <jsaloni484@gmail.com>
2020-05-07 21:24:09 +0530

    added passport-jwt

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -1,5 +1,5 @@
 const passport=require('passport');
-const passportJWT=require('passport-jwt').Strategy;
+const JWTSrategy=require('passport-jwt').Strategy;
 
 const ExtractJWT=require('passport-jwt').ExtractJwt;
 const User=require('../models/user');
@@ -8,3 +8,20 @@ let opts={
     jwtFormRequest:ExtractJWT.fromAuthHeaderAsBearerToken,
     secretOrKey='codeial'
 }
+
+passport.use(new JWTSrategy(opts,function(jwtPayLoad,done){
+
+    User.findById(jwtPayLoad._id,function(err,done){
+        if(err){
+            console.log("error is finding user using jwt");
+            return (err);
+        }
+        if(user){
+            return done(null,user);
+        }
+        else{
+            return done(null,false);
+        }
+
+    })
+}));
